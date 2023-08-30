ae803be11a1dcb32cb7f2d656591fc029978e791
Hemant Verma <vermahemant837@gmail.com>
2023-08-30 11:02:48 +0530

    Expense Tracker v 0.1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import NewExpense from "./components/NewExpense/NewExpense";
+import Expenses from "./components/Expenses/Expense";
 
-function App() {
+const dummyData = [
+  {
+    id: "e1",
+    expenseDate: new Date(2022, 8, 23),
+    expenseTitle: "Burger",
+    expenseAmount: 12,
+  },
+  {
+    id: "e2",
+    expenseDate: new Date(2023, 8, 22),
+    expenseTitle: "Pizza",
+    expenseAmount: 10,
+  },
+  {
+    id: "e3",
+    expenseDate: new Date(2021, 8, 22),
+    expenseTitle: "Petrol",
+    expenseAmount: 15,
+  },
+  {
+    id: "e4",
+    expenseDate: new Date(2023, 8, 22),
+    expenseTitle: "Gadar 2",
+    expenseAmount: 20,
+  },
+];
+
+const App = () => {
+  const [expenses, setExpenses] = useState(dummyData)
+  const addExpenseHandler = (expense) => {
+    setExpenses((prevExpenses)=> {
+      return [...prevExpenses, expense]
+    })
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NewExpense onAddExpense={addExpenseHandler} />
+      <Expenses items={expenses} />
     </div>
   );
-}
+};
 
 export default App;
